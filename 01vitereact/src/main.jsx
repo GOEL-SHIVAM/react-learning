05432a755f3883dbab9b19441822beb3355359e2
GOEL-SHIVAM <131146872+GOEL-SHIVAM@users.noreply.github.com>
2025-01-24 11:28:01 +0530

    lec 4

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Name(params) {
+  return (
+    <div>
+      <h1>My name is shivam goel</h1>
+    </div>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Name />
+  </StrictMode>
 )
