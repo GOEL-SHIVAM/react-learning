2ab01e4587e15ad8ae29728e02c0bc187360fd24
GOEL-SHIVAM <131146872+GOEL-SHIVAM@users.noreply.github.com>
2025-02-14 18:39:07 +0530

    react router

---
+++ react router/src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
